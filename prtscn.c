71554e2d7a080418cd096e16c9b8e04442421366
dchadwick <dchadwick@ara.com>
2019-07-22 14:22:10 -0400

    initial commit

---
+++ prtscn.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <X11/X.h>
+#include <X11/Xlib.h>
+
+void getScreen(const int, const int, const int, const int, unsigned char *);
+void getScreen(const int xx, const int yy, const int W, const int H, /*out*/ unsigned char * data)
+{
+    
+}
