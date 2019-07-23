e49064c511d9ad7cd955dc3e27e2d14c7903d0f6
dchadwick <dchadwick@ara.com>
2019-07-23 11:44:31 -0400

    switched to using Pillow for Windows solution

--- prtscn.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <X11/X.h>
-#include <X11/Xlib.h>
-
-void getScreen(const int, const int, const int, const int, unsigned char *);
-void getScreen(const int xx, const int yy, const int W, const int H, /*out*/ unsigned char * data)
-{
-    
-}
