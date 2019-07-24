3219c83819ab23b003f3caac84cc4d1ded31efd5
Daquaris Chadwick <sourhead5@gmail.com>
2019-07-23 21:14:42 -0400

    added league of legends application screenshot code

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from PIL import Image
+from PIL import Image, ImageGrab
 from pytesseract import Output
 import pytesseract
+import win32gui, win32com.client
+
+hwnd = win32gui.FindWindow(None, "League of Legends")
+
+# Send alt keys
+shell = win32com.client.Dispatch("WScript.Shell")
+shell.SendKeys('%')
+
+win32gui.SetForegroundWindow(hwnd)
+bbox = win32gui.GetWindowRect(hwnd)
+img = ImageGrab.grab(bbox)
+img = img.resize((6398, 3600), Image.ANTIALIAS)
+img.save("image_300.jpg", "JPEG", dpi=(300, 300))
 
 def output_to_array(output):
     ret = []
     word = ""
     for c in output:
         if c is '\n':
-            if "joined the room" in word:
+            if "joined the lobby" in word:
                 ret.append(word)    
             word = ""
             continue
@@ -18,14 +31,16 @@ def output_to_array(output):
 def get_players(list):
     players = []
     for word in list:
-        ind = word.index("joined the room")
+        ind = word.index("joined")
         player = word[:ind-1]
         if player not in players:
             players.append(player)
     return players
 
-#im = Image.open("lolqueue_example.jpg")
-output_dict = pytesseract.image_to_string("lolqueue_example_300.jpg", config=r'-l eng --psm 3 --dpi 300')
-output_list = output_to_array(output_dict)
-players = get_players(output_list)
-print("Players are: ", players)
+def read_screenshot():
+    output_dict = pytesseract.image_to_string("image_300.jpg", config=r'-l eng --psm 11 --dpi 300')
+    output_list = output_to_array(output_dict)
+    players = get_players(output_list)
+    print("Players are: ", players)
+
+read_screenshot()
